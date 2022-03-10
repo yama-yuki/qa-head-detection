443dbc9a4e2820d05b5492be16ea523dfa8f6184
Yamamoto Yuki <yuki-yama@elm14.naist.jp>
2022-03-10 17:11:26 +0900

    Updated evalq.py

--- scripts/main.py
+++ scripts/main.py
@@ -93,10 +93,16 @@ def main():
         # Downloading and loading a dataset from the hub.
         #raw_datasets = load_dataset(args.dataset_name, args.dataset_config_name)
         #data_files = {'train': '../data/squad/train-v1.1.json', 'validation': '../data/squad/dev-v1.1.json'}
-        data_files = {'train': '', 'validation': '',}
-        data_files['train'] = args.custom_train_data
-        data_files['validation'] = args.custom_dev_data
-
+        if (args.custom_train_data and args.custom_dev_data):
+            data_files = {'train': '', 'validation': ''}
+            data_files['train'] = args.custom_train_data
+            data_files['validation'] = args.custom_dev_data
+        elif args.custom_test_data:
+            data_files = {'test': ''}
+            data_files['test'] = args.custom_test_data
+        else:
+            sys.exit()
+        logger.info('hoge')
         raw_datasets = load_dataset('squad.py', data_files=data_files)
 
     else:
@@ -116,6 +122,8 @@ def main():
     #
     # In distributed training, the .from_pretrained methods guarantee that only one local process can concurrently
     # download model & vocab.
+
+    
 
     if args.config_name:
         config = AutoConfig.from_pretrained(args.config_name)
@@ -613,6 +621,9 @@ def parse_args():
     )
     parser.add_argument(
         "--custom_dev_data", type=str, default=None, help="A json file containing the validation data."
+    )
+    parser.add_argument(
+        "--custom_test_data", type=str, default=None, help="A json file containing the validation data."
     )
     parser.add_argument(
         "--dataset_config_name",
--- scripts/predict.py
+++ scripts/predict.py
@@ -1,47 +1,78 @@
 import argparse
 import csv
+import json
+import os
+import sys
 import torch
 from transformers import BertForQuestionAnswering, AutoTokenizer, AutoConfig
-
-parser = argparse.ArgumentParser()
-parser.add_argument('--model_dir')
-parser.add_argument('--pretrained', default='bert-base-uncased')
-parser.add_argument('--input_path')
-parser.add_argument('--output_path')
-args = parser.parse_args() 
-model_directory = args.model_dir
-pretrained_model = args.pretrained
-in_path = args.input_path
-out_path = args.output_path
-
-config = AutoConfig.from_pretrained(model_directory + "/config.json")
-tokenizer_config = AutoConfig.from_pretrained(model_directory + "/tokenizer_config.json")
-
-model = BertForQuestionAnswering.from_pretrained(pretrained_model, config=config)
-model.load_state_dict(torch.load(model_directory + "/pytorch_model.bin", map_location=torch.device('cuda')))
-tokenizer = AutoTokenizer.from_pretrained(pretrained_model, config=tokenizer_config)
-
-def predict(question, context):
-    ## predict
-    inputs = tokenizer.encode_plus(question, context, add_special_tokens=True, return_tensors="pt")
-    input_ids = inputs["input_ids"].tolist()[0]
-    output = model(**inputs)
-    answer_start = torch.argmax(output.start_logits)  
-    answer_end = torch.argmax(output.end_logits) + 1 
-    answer = tokenizer.convert_tokens_to_string(tokenizer.convert_ids_to_tokens(input_ids[answer_start:answer_end]))
-    return answer
+from eval import evaluate
 
 def main():
-    with open(in_path,mode='r',encoding='utf-8') as f:
-        with open(out_path,mode='w',encoding='utf-8') as o:
-            reader = csv.reader(f, delimiter='\t')
-            writer = csv.writer(o, delimiter='\t')
+    ## parse arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--mode', default='pred')
+    parser.add_argument('--model_dir')
+    parser.add_argument('--pretrained', default='bert-base-uncased')
+    parser.add_argument('--input_path', hint='.json')
+    parser.add_argument('--output_path', hint='.json')
+    args = parser.parse_args()
 
-            for row in reader:
-                context = row[0]
-                questions = row[1:]
-                answers = [predict(question, context) for question in questions]
-                writer.writerow(answers)
+    mode = args.mode
+    model_directory = args.model_dir
+    pretrained_model = args.pretrained
+
+    ## load model & tokenizer
+    config = AutoConfig.from_pretrained(os.path.join(model_directory, "config.json"))
+    tokenizer_config = AutoConfig.from_pretrained(os.path.join(model_directory, "tokenizer_config.json"))
+
+    model = BertForQuestionAnswering.from_pretrained(pretrained_model, config=config)
+    model.load_state_dict(torch.load(os.path.join(model_directory, "pytorch_model.bin"), map_location=torch.device('cuda')))
+    tokenizer = AutoTokenizer.from_pretrained(pretrained_model, config=tokenizer_config)
+
+    def predict(question, context):
+
+        inputs = tokenizer.encode_plus(question, context, add_special_tokens=True, return_tensors="pt")
+        input_ids = inputs["input_ids"].tolist()[0]
+        output = model(**inputs)
+        answer_start = torch.argmax(output.start_logits)  
+        answer_end = torch.argmax(output.end_logits) + 1 
+        answer = tokenizer.convert_tokens_to_string(tokenizer.convert_ids_to_tokens(input_ids[answer_start:answer_end]))
+
+        return answer
+
+    if mode == 'pred':
+        ## predict & write out
+        in_path = args.input_path
+        out_path = args.output_path
+
+        with open(in_path,mode='r',encoding='utf-8') as f:
+            with open(out_path,mode='w',encoding='utf-8') as o:
+                reader = csv.reader(f, delimiter='\t')
+                writer = csv.writer(o, delimiter='\t')
+
+                for row in reader:
+                    context = row[0]
+                    questions = row[1:]
+                    answers = [predict(question, context) for question in questions]
+                    writer.writerow(answers)
+    
+    elif mode == 'test':
+        ## evaluate on test set
+        test_path = args.test_path
+        pred_path = 'pred.json'
+        
+        with open(test_path, mode='r', encoding='utf-8') as f:
+            dataset_json = json.load(f)
+            dataset = dataset_json['data']
+        with open(pred_path, mode='r', encoding='utf-8') as g:
+            predictions = json.load(g)
+
+        print(json.dumps(evaluate(dataset, predictions)))
+
+    else:
+        sys.exit('SPECIFY MODE')
+
+
 
 if __name__ == '__main__':
     main()
